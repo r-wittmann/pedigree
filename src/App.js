7fa934c2f431002841185d893f4e918bb152bbd7
Rainer Wittmann <r-wittmann@outlook.de>
2020-01-11 18:18:44 +0100

    implement fuzzy search on first name, last name and dissertation title. results are written to console only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from "./components/SearchBar";
+import Fuse from "fuse.js";
 
 const personData = require("./data/person_data");
 
@@ -17,10 +18,34 @@ class App extends React.Component {
         this.setState({
             searchTerm
         });
+
+        if (searchTerm.length < 3) return;
+
+        let options = {
+            shouldSort: true,
+            threshold: 0.2,
+            location: 0,
+            distance: 5,
+            maxPatternLength: 32,
+            minMatchCharLength: 3,
+        };
+
+        let resultObject = {
+            firstNameResult: this.fuseSearch(searchTerm, options, ["fistName"]),
+            lastNameResult: this.fuseSearch(searchTerm, options, ["lastName"]),
+            dissertationTitleResult: this.fuseSearch(searchTerm, options, ["dissertationTitle"]),
+        };
+
+        console.log(resultObject);
+
+    };
+
+    fuseSearch = (searchTerm, options, keys) => {
+        let fuse = new Fuse(personData, Object.assign({}, options, {keys}));
+        return fuse.search(searchTerm, {limit: 10});
     };
 
     render() {
-        console.log(personData[3000].firstName, personData[3000].lastName);
         return (
             <div>
                 <SearchBar searchTerm={this.state.searchTerm} onChange={this.onSearchTermChange}/>
