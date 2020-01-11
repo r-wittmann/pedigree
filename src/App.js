f8b56dd821a7e417b7832de0c9cc13425397b54d
Rainer Wittmann <r-wittmann@outlook.de>
2020-01-11 16:26:16 +0100

    add bootstrap styling to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       Test
+        <Button>Test</Button>
     </div>
   );
 }
